a945828bc7c1473708de4fdb1c57537a61a8733d
zeenlinux <pedro.modesto15@gmail.com>
2021-09-19 17:29:35 -0300

    teste de java

---
+++ src/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,14 @@
+package com.dio;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub para aparecer isso de cima main ctrl+space
+		int x = 1;
+		int y = 3;
+		int w = x+y;
+		System.out.println("primeiro programa"+x+y+w);
+
+	}
+
+}
---
+++ src/com/dio/segundo.java
@@ -0,0 +1,24 @@
+package com.dio;
+
+public class segundo {
+	private String nome;
+	private String cor;
+	private Integer idade;
+	public segundo() {}
+	public segundo(String nome, String cor, Integer idade) {
+		this.nome = nome;
+		this.cor = cor;
+		this.idade = idade;
+	}
+	
+	
+	/*public Gato(String nome, String cor, Integer idade) {
+		this.nome= nome;
+		this.cor= cor;
+		this.idade= odade;
+	}*/
+	
+	
+	
+	
+}
